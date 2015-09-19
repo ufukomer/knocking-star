5d10f3dff917e2dabc61c95ccd64aebe1e5e6e88
ufukomer <ufukomer@hotmail.com>
2015-09-19 09:56:56 +0300

    init commit

---
+++ README.md
@@ -0,0 +1,1 @@
+#Knocking Star
---
+++ src/main/java/com/knockingstar/KnockingStar.java
@@ -0,0 +1,15 @@
+package com.knockingstar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Ufuk on 19-09-15.
+ */
+@SpringBootApplication
+public class KnockingStar {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KnockingStar.class, args);
+    }
+}
